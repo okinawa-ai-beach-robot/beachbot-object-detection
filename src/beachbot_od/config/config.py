0dc186ae78a3c212123246829856e95d2a381ccf
Christopher Buckley <topherbuckley@gmail.com>
2024-10-31 18:33:52 +0900

    wip general config.load_config

--- src/beachbot_od/config/config.py
+++ src/beachbot_od/config/config.py
@@ -3,6 +3,8 @@ import os
 import beachbot_od
 from pathlib import Path
 import logging
+from enum import Enum
+import shutil
 
 logging = logging.getLogger("beachbot_od")
 
@@ -58,3 +60,39 @@ logging.info(f"BEACHBOT_CONFIG: {BEACHBOT_CONFIG}")
 logging.info(f"BEACHBOT_LOGS: {BEACHBOT_LOGS}")
 logging.info(f"BEACHBOT_MODELS: {BEACHBOT_MODELS}")
 logging.info(f"BEACHBOT_DATASETS: {BEACHBOT_DATASETS}")
+
+
+class CONFIG_TYPE(Enum):
+    ROBOFLOW = 1
+    HF_MODEL_CARD = 2
+
+
+def load_config(config_type: CONFIG_TYPE, config_path: Path = None) -> dict:
+    """
+    config_type: CONFIG_TYPE
+    config_path: Path (Optional) use to override default config file locations with a custom one
+    """
+
+    if config_path is None:
+        if config_type == 1:
+            filename = "roboflow_version_config.yaml"
+            config_path = Path(BEACHBOT_CONFIG / filename)
+            config_src_path = Path(BEACHBOT_OD_PATH) / "config" / filename
+        if config_type == 2:
+            filename = "roboflow_version_config.yaml"
+            config_path = Path(BEACHBOT_CONFIG / filename)
+            config_src_path = Path(BEACHBOT_OD_PATH) / "config" / filename
+        logging.info(
+            f"Config file not specified, using default location of {config_path}."
+        )
+        if not config_path.exists():
+            # Copy file from beachbot_od to user config directory
+            logging.info(
+                f"Config file not found at {config_path}. Copying from {config_src_path}."
+            )
+            shutil.copyfile(config_src_path, config_path)
+
+    # Load the YAML configuration file
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
