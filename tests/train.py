1c05c06b9ca9df58d1b10f401c793e82560d7405
Christopher Buckley <topherbuckley@gmail.com>
2024-11-01 15:09:52 +0900

    Updating training test to use SupportedModels

--- tests/train.py
+++ tests/train.py
@@ -1,5 +1,11 @@
 from beachbot_od.yolo_v5_api.train import train_model
+from beachbot_od.utils.models import SupportedModels
+
 
 train_model(
-    model_format="yolov5s", img_width=160, dataset_version=13, epochs=1, overwrite=True
+    model_format=SupportedModels.YOLOV5S,
+    img_width=160,
+    dataset_version=13,
+    epochs=1,
+    overwrite=True,
 )
