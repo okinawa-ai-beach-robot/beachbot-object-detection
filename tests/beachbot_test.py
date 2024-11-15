adb099ba07aa7bf7d260d899e86e571a412485e6
Christopher Buckley <topherbuckley@gmail.com>
2024-11-15 14:40:45 +0900

    Updating pytest config and adding more rigid config test

--- tests/beachbot_test.py
+++ tests/beachbot_test.py
@@ -1,4 +1,12 @@
-def test_beachbot_import():
+def test_beachbot_import(caplog):
+    # Import your module and logger
     from beachbot.config import config, logger
 
-    logger.info("BEACHBOT_HOME: " + str(config.BEACHBOT_HOME))
+    # Access the config variable
+    beachbot_home = config.BEACHBOT_HOME
+
+    # Log the message
+    logger.info("BEACHBOT_HOME: " + str(beachbot_home))
+
+    # Check if the correct log message was recorded
+    assert "BEACHBOT_HOME: " + str(beachbot_home) in caplog.text
